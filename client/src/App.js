b34827a909b7606dde3afebe78e29da66dab555e
unknown <dzhitser@example.com>
2020-11-07 20:50:27 +0300

    add style

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {useState} from 'react';
-//import 'index.css';
-import ToDoList from './ToDoList'
+import '../src/index.css';
+import ToDoList from './ToDoList';
+import 'materialize-css'; // It installs the JS asset only
+import 'materialize-css/dist/css/materialize.min.css';
+
 
 function App() {
 
@@ -20,10 +23,24 @@ function App() {
 
   return (
     <div>
-      <h1> ToDo List</h1>
+      <h3> ToDo List</h3>
+        <div className="row">
+          <div className="input-field col s6">
+           <div className="but-inp">
+            
+            <input value={inputList}  onChange={itemEvent} id="first_name2" type="text" className="validate"/>
+            &nbsp;<button onClick={listItems}> + </button>
+            {/* <label class="active" for="first_name2">Введите задание</label> */}
+            </div> 
+          </div>
+        </div>
       <br />
-      <input type="text" placeholder="Введите задание" value={inputList} onChange={itemEvent}/>
-      <button onClick={listItems}> + </button>
+
+
+      {/* <input type="text" placeholder="Введите задание" value={inputList} onChange={itemEvent}/> */}
+
+
+      
 
       <ul>
         <li>
